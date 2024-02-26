93b13cd8d15b21c7a95ff08aba9e221d54b178e5
Besa <joao.pereira@precato.com.br>
2024-02-26 13:42:08 -0300

    add column mapping and constraints

--- Models/Url.cs
+++ Models/Url.cs
@@ -1,20 +1,24 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Shortener.Models
 {
     [Table("urls")]
+    [Index(nameof(ShortUrl), IsUnique = true)]
     public class Url
     {
         public Guid Id { get; set; }
 
-        [StringLength(4, MinimumLength = 4)]
         [Column("short_url")]
+        [StringLength(4, MinimumLength = 4)]
+        [Required]
         public string ShortUrl { get; set; }
 
         [Column("original_url")]
+        [Required]
         public string OriginalUrl { get; set; }
-        
+
         public int Visits { get; set; }
         public DateTime CreatedAt { get; set; }
     }
