12a750854c303c38689415a499fe0c0ff5c4421f
Besa <joao.pereira@precato.com.br>
2024-02-26 13:31:27 -0300

    map url attributes

--- Models/Url.cs
+++ Models/Url.cs
@@ -1,10 +1,20 @@
-namespace Shortener.Entities
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shortener.Models
 {
+    [Table("urls")]
     public class Url
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
+
+        [StringLength(4, MinimumLength = 4)]
+        [Column("short_url")]
         public string ShortUrl { get; set; }
+
+        [Column("original_url")]
         public string OriginalUrl { get; set; }
+        
         public int Visits { get; set; }
         public DateTime CreatedAt { get; set; }
     }
