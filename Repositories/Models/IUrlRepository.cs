76894561fb7e97632abcfbd13e6dbc81ab6b06b0
Besa <joao.pereira@precato.com.br>
2024-02-26 15:37:12 -0300

    add null returns to methods

--- Repositories/Models/IUrlRepository.cs
+++ Repositories/Models/IUrlRepository.cs
@@ -5,8 +5,8 @@ namespace Shortener.Repositories.Models
     public interface IUrlRepository
     {
         Task<Url> Add(Url url);
-        Task<Url> Find(string url);
-        Task<Url> Delete(string url);
-        Task<Url> FindByUserId(Guid id);
+        Task<Url?> Find(string url);
+        Task<Url?> Delete(string url);
+        Task<Url?> FindByUserId(Guid id);
     }
 }
