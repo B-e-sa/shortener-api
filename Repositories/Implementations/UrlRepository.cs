7ab9153594c91ebb83c50fc3f739ab11b1550e38
Besa <joao.pereira@precato.com.br>
2024-02-26 13:41:40 -0300

    init url repository

---
+++ Repositories/Implementations/UrlRepository.cs
@@ -0,0 +1,36 @@
+using Shortener.Data;
+using Shortener.Models;
+using Shortener.Repositories.Models;
+
+namespace Shortener.Repositories
+{
+    public class UrlRepository : IUrlRepository
+    {
+        private readonly ShortenerDbContext _dbContext;
+
+        public UrlRepository(ShortenerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<Url> Add(Url url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Url> Delete(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Url> Find(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Url> FindByUserId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repositories/Models/IUrlRepository.cs
@@ -0,0 +1,12 @@
+using Shortener.Models;
+
+namespace Shortener.Repositories.Models
+{
+    public interface IUrlRepository
+    {
+        Task<Url> Add(Url url);
+        Task<Url> Find(string url);
+        Task<Url> Delete(string url);
+        Task<Url> FindByUserId(Guid id);
+    }
+}
