c6865d97663e6fdcaa6c628f2c40df3c0805d825
Besa <joao.pereira@precato.com.br>
2024-02-26 14:53:43 -0300

    add url service & repository

--- Program.cs
+++ Program.cs
@@ -1,8 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Shortener.Data;
+using Shortener.Repositories;
+using Shortener.Repositories.Models;
+using Shortener.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// SERVICES
+builder.Services.AddScoped<UrlService>();
+
+// REPOSITORIES
+builder.Services.AddScoped<IUrlRepository, UrlRepository>();
+
+// DB
+builder.Services.AddDbContext<ShortenerDbContext>(x =>
+    {
+        x.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+    }
+);
 
 var app = builder.Build();
 
