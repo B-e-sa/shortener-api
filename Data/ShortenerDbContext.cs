1c63562543cc0faf54fbe9da260276c84de940bb
Besa <joao.pereira@precato.com.br>
2024-02-25 23:34:06 -0300

    init db context

---
+++ Data/ShortenerDbContext.cs
@@ -0,0 +1,14 @@
+using Shortener.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shortener.Data
+{
+    public class ShortenerDbContext : DbContext
+    {
+        public ShortenerDbContext(DbContextOptions<ShortenerDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Url> Urls { get; set; }
+    }
+}
