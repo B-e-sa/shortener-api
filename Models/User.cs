415cd4aafd52e9dbaf2a9e3343cbad96d243fd83
B-e-sa <joaospereira963@gmail.com>
2024-02-29 16:47:42 -0300

    implement string.Empty on User password field

--- Models/User.cs
+++ Models/User.cs
@@ -30,7 +30,7 @@ namespace Shortener.Models
         [RegularExpression(
             @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
         )]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
 
         [Column("created_at")]
         public DateTime CreatedAt { get; set; }
