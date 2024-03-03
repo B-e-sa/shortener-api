bda2d75a0543ccc62270ec1a18ce2dd48c0c275f
B-e-sa <joaospereira963@gmail.com>
2024-03-03 02:57:17 -0300

    add invalid url test

--- Test/CreateUrlControllerTest.cs
+++ Test/CreateUrlControllerTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Shortener.Controllers;
+using Shortener.Controllers.ResponseHandlers.ErrorHandlers;
 using Shortener.Controllers.ResponseHandlers.SuccessHandlers;
 using Shortener.Models;
 using Shortener.Services.Models;
@@ -15,19 +16,19 @@ namespace Shortener.Tests.Controllers
         {
             // Arrange
             var dummyUrl = Faker.Internet.Url();
-            var createUrlServiceMock = new Mock<ICreateUrlService>();
+            var mockCreateUrlService = new Mock<ICreateUrlService>();
             var createdUrl = new Url
             {
                 OriginalUrl = dummyUrl,
                 ShortUrl = "abcd"
             };
 
-            createUrlServiceMock
+            mockCreateUrlService
                 .Setup(x => x.Handle(It.IsAny<Url>()))
                 .ReturnsAsync(createdUrl);
 
             var request = new CreateUrlRequest { OriginalUrl = dummyUrl };
-            var controller = new CreateUrlController(createUrlServiceMock.Object);
+            var controller = new CreateUrlController(mockCreateUrlService.Object);
 
             // Act
             var result = await controller.Handle(request);
@@ -37,5 +38,25 @@ namespace Shortener.Tests.Controllers
             var createdHandlerResult = Assert.IsType<CreatedHandler>(createdResult.Value);
             Assert.IsType<Url>(createdHandlerResult.Value);
         }
+
+        [Fact]
+        public async Task Handle_InvalidUrl_ReturnsBadRequest()
+        {
+            // Arrange
+            var invalidUrl = Faker.Lorem.Sentence();
+            var request = new CreateUrlRequest { OriginalUrl = invalidUrl };
+            var createUrlServiceMock = new Mock<ICreateUrlService>();
+
+            var controller = new CreateUrlController(createUrlServiceMock.Object);
+            controller.ModelState.AddModelError("OriginalUrl", "Bad request error");
+
+            // Act
+            var result = await controller.Handle(request);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        // TODO: implement case if user.id is not present
     }
 }
