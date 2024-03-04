796352c58766cde0cd734ba53f9707b2f1886dfa
B-e-sa <joaospereira963@gmail.com>
2024-03-04 14:38:27 -0300

    init create user controller

---
+++ Controllers/User/CreateUserController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shortener.Controllers.User
+{
+    [ApiController]
+    [Route("/user")]
+    public class CreateUserController : ControllerBase
+    {
+        
+    }
+}
