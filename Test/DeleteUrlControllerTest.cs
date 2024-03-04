d059ecc5e11551d0b5af2d70f71c67cb474c4aa7
B-e-sa <joaospereira963@gmail.com>
2024-03-04 00:45:26 -0300

    add success controller handler test to delete url controller

--- Test/DeleteUrlControllerTest.cs
+++ Test/DeleteUrlControllerTest.cs
@@ -1,7 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Shortener.Controllers;
+using Shortener.Controllers.ResponseHandlers.SuccessHandlers;
+using Shortener.Models;
+using Shortener.Services.Models;
+using Xunit;
+
 namespace Shortener.Test
 {
     public class DeleteUrlControllerTest
     {
-        
+        [Fact]
+        public async Task Handle_ExistentUrl_ReturnsFoundUrl()
+        {
+            // Arrange
+            var mockDeleteUrlService = new Mock<IDeleteUrlService>();
+            var mockFindUrlByIdService = new Mock<IFindUrlByIdService>();
+            var urlId = Guid.NewGuid();
+            var foundUrl = new Url {
+                Id = urlId
+            };
+
+            mockFindUrlByIdService
+                .Setup(x => x.Handle(urlId))
+                .ReturnsAsync(foundUrl);
+
+            mockDeleteUrlService
+                .Setup(x => x.Handle(foundUrl))
+                .ReturnsAsync(foundUrl);
+
+            var request = new DeleteUrlRequest { Id = urlId.ToString() };
+            var deleteUrlController = new DeleteUrlController(
+                mockDeleteUrlService.Object,
+                mockFindUrlByIdService.Object
+            );
+
+            // Act
+            var result = await deleteUrlController.Handle(request);
+
+            // Assert
+            var deletedResult = Assert.IsType<OkObjectResult>(result);
+            var deletedHandlerResult = Assert.IsType<SuccessHandler>(deletedResult.Value);
+            Assert.IsType<Url>(deletedHandlerResult.Value);
+        }
+
+        [Fact]
+        public void Handle_InexistentUrl_ReturnsNotFound()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+        }
     }
 }
